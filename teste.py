2e6c60127b408a2cf75005799444e31b3a3e2c20
Joao Assalim <joassalim@gmail.com>
2024-08-06 11:38:23 -0300

    teste do pre commit

--- teste.py
+++ teste.py
@@ -2,7 +2,7 @@
 
 
 # Exemplo de código que precisa de formatação
-def some_function(param1, param2, param3, param4):
+def some_function(param1, param2):
     if param1 and param2:
         print("Condition met!")
     else:
@@ -11,7 +11,9 @@ def some_function(param1, param2, param3, param4):
 
 # Exemplo de função que tem uma linha longa para testar a configuração max-line-length
 def long_line_example():
-    very_long_string = "This is an example of a very long string that should exceed the max line length and thus trigger formatting or linting issues in order to test the pre-commit hooks properly."
+    very_long_string = """This is an example of a very long string that should exceed the max line
+    length and thus trigger
+    formatting or linting issues in order to test the pre-commit hooks properly."""
     print(very_long_string)
 
 
@@ -23,4 +25,5 @@ def unused_variables():
 
 
 # Comentário de linha muito longa para testar o max-line-length
-# Este é um comentário muito longo que deve ultrapassar o comprimento máximo de linha definido pelo Flake8, que está configurado para 120 caracteres neste exemplo de arquivo.
+# Este é um comentário muito longo que deve ultrapassar o comprimento máximo de linha definido
+# pelo Flake8, que está configurado para 120 caracteres neste exemplo de arquivo.
